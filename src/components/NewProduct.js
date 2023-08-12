f63c098dbe5bf3fcccc84c0fcfaa808a76186390
HarhslaMjn <mahajanh006@gmail.com>
2023-08-12 22:42:45 +0530

    #

---
+++ src/components/NewProduct.js
@@ -0,0 +1,12 @@
+import './NewProduct.css'
+import ProductForm from './ProductForm';
+
+function NewProduct() {
+  return ( <div className='New-Product'>
+    <ProductForm/>
+  </div>
+
+  )
+}
+
+export default NewProduct;
